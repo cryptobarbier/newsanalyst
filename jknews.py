3f834fb0636b22d7622bf6c1bf54c4536e9a6ccc
cryptobarbier <54747784+cryptobarbier@users.noreply.github.com>
2019-11-23 23:18:52 +0000

    Add files via upload

--- jknews.py
+++ jknews.py
@@ -12,6 +12,7 @@ from sklearn.metrics.pairwise import linear_kernel
 from sklearn.cluster import AgglomerativeClustering
 from summarizer import Summarizer
 from docx import Document
+import docx
 from docx.shared import Pt
 
 
@@ -92,12 +93,57 @@ class NewsArticle():
         self.summary=summ
         self.subtitles=subtitles
         self.urls=urls
-        
+
+def add_hyperlink(paragraph, url, text, color, underline):
+    """
+    A function that places a hyperlink within a paragraph object.
+
+    :param paragraph: The paragraph we are adding the hyperlink to.
+    :param url: A string containing the required url
+    :param text: The text displayed for the url
+    :return: The hyperlink object
+    """
+
+    # This gets access to the document.xml.rels file and gets a new relation id value
+    part = paragraph.part
+    r_id = part.relate_to(url, docx.opc.constants.RELATIONSHIP_TYPE.HYPERLINK, is_external=True)
+
+    # Create the w:hyperlink tag and add needed values
+    hyperlink = docx.oxml.shared.OxmlElement('w:hyperlink')
+    hyperlink.set(docx.oxml.shared.qn('r:id'), r_id, )
+
+    # Create a w:r element
+    new_run = docx.oxml.shared.OxmlElement('w:r')
+
+    # Create a new w:rPr element
+    rPr = docx.oxml.shared.OxmlElement('w:rPr')
+
+    # Add color if it is given
+    if not color is None:
+      c = docx.oxml.shared.OxmlElement('w:color')
+      c.set(docx.oxml.shared.qn('w:val'), color)
+      rPr.append(c)
+
+    # Remove underlining if it is requested
+    if not underline:
+      u = docx.oxml.shared.OxmlElement('w:u')
+      u.set(docx.oxml.shared.qn('w:val'), 'none')
+      rPr.append(u)
+
+    # Join all the xml elements together add add the required text to the w:r element
+    new_run.append(rPr)
+    new_run.text = text
+    hyperlink.append(new_run)
+
+    paragraph._p.append(hyperlink)
+
+    return hyperlink
+
     def TextOutput(self,title,docname):
           document = Document()
           style = document.styles['Normal']
           font = style.font
-          font.name = 'Times New Roman'
+          font.name = 'Calibri'
           font.size = Pt(10)
           # Write the title passed at the function
           document.add_heading(title, 0)
@@ -105,7 +151,8 @@ class NewsArticle():
           for element in self.summary:
             document.add_heading(self.subtitles[i], level=1)
             p = document.add_paragraph()
-            hyperlink = p.add_hyperlink(text=self.urls[i], url=self.urls[i])
+            #add a hyperlink with the normal formatting (blue underline)
+            hyperlink = add_hyperlink(p, self.urls[i], self.urls[i], None, True)
             p = document.add_paragraph(self.summary[element])
             i=i+1
           document.save(docname+'.docx')
